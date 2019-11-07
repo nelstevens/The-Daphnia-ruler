56aa67e4ebab2c7da055f8c2bd62e8a1363ffa4f
nelstevens <nelson.stevens92@gmail.com>
2019-11-07 18:21:53 +0100

    enabled multiple objects without erosion

--- measurement_methods.py
+++ measurement_methods.py
@@ -49,9 +49,9 @@ def head_method(image):
     # create mask
     edges_mag = scharr(gray)
     edges_med = np.median(edges_mag)
-    edges_thresh = 2.5*edges_med
+    edges_thresh = 5.5*edges_med
     edges = edges_mag >= edges_thresh
-    edges = morphology.closing(edges, morphology.square(3))
+    #edges = morphology.closing(edges, morphology.square(3))
     edges = ndimage.binary_fill_holes(edges)
     edges = morphology.erosion(edges, morphology.square(3))
 
@@ -65,6 +65,7 @@ def head_method(image):
     edges = np.uint8(edges)
     edges_res = edges
     kernel_size = 2
+    '''
     # continue opening until solidity fits
     while props[0].solidity < 0.93:
         edges_res = edges
@@ -76,79 +77,81 @@ def head_method(image):
         props = measure.regionprops(label_img, gray, coordinates = 'xy')
         props = sorted(props, reverse=True, key=lambda k: k.area)
         kernel_size += 1
+    '''
+    for i in range(len(props)):
+        # plot binary image
+        bw_img = 0*edges_res
+        bw_img = (label_img) == props[i].label
+        bw_img_all = bw_img.copy()
+        bw_img_all = bw_img_all + ((label_img) == props[i].label)
+        binary2 = np.uint8(255*bw_img_all)
 
-    # plot binary image
-    bw_img = 0*edges_res
-    bw_img = (label_img) == props[0].label
-    bw_img_all = bw_img.copy()
-    bw_img_all = bw_img_all + ((label_img) == props[0].label)
-    binary2 = np.uint8(255*bw_img_all)
+        # plot mask contour on image
+        contours, hierarchy = cv2.findContours(binary2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(img, contours, -1, (0, 0, 255), 1)
 
-    # plot mask contour on image
-    contours, hierarchy = cv2.findContours(binary2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img, contours, -1, (0, 0, 255), 1)
+        # plot elipse on image
+        cv2.ellipse(img, (int(props[i].centroid[1]), int(props[i].centroid[0])),
+                    (int(props[i].major_axis_length/2), int(props[i].minor_axis_length/2)),
+                    -props[i].orientation*180/pi, 0, 360, 0, 0)
 
-    # plot elipse on image
-    cv2.ellipse(img, (int(props[0].centroid[1]), int(props[0].centroid[0])),
-                (int(props[0].major_axis_length/2), int(props[0].minor_axis_length/2)),
-                -props[0].orientation*180/pi, 0, 360, 0, 0)
+        # # plot major axis of fitted elipse
+        # get deltas
+        dx = (props[i].major_axis_length/2)*math.sin(props[i].orientation)
+        dy = (props[i].major_axis_length/2)*math.cos(props[i].orientation)
 
-    # # plot major axis of fitted elipse
-    # get deltas
-    dx = (props[0].major_axis_length/2)*math.sin(props[0].orientation)
-    dy = (props[0].major_axis_length/2)*math.cos(props[0].orientation)
+        # get start
+        x1 = props[i].centroid[0]-dx
+        y1 = props[i].centroid[1]+dy
 
-    # get start
-    x1 = props[0].centroid[0]-dx
-    y1 = props[0].centroid[1]+dy
+        # get end
+        x2 = props[i].centroid[0]+dx
+        y2 = props[i].centroid[1]-dy
 
-    # get end
-    x2 = props[0].centroid[0]+dx
-    y2 = props[0].centroid[1]-dy
+        # plot line
+        cv2.line(img, (int(y2), int(x2)), (int(y1), int(x1)), (0, 255, 0), 2)
 
-    # plot line
-    cv2.line(img, (int(y2), int(x2)), (int(y1), int(x1)), (0, 255, 0), 2)
+        # # plot minor axis of fitted elipse
+        # get deltas
+        dx = (props[i].minor_axis_length/2)*math.sin(props[i].orientation-(math.pi/2))
+        dy = (props[i].minor_axis_length/2)*math.cos(props[i].orientation-(math.pi/2))
 
-    # # plot minor axis of fitted elipse
-    # get deltas
-    dx = (props[0].minor_axis_length/2)*math.sin(props[0].orientation-(math.pi/2))
-    dy = (props[0].minor_axis_length/2)*math.cos(props[0].orientation-(math.pi/2))
+        # get start
+        x1 = props[i].centroid[0]-dx
+        y1 = props[i].centroid[1]+dy
 
-    # get start
-    x1 = props[0].centroid[0]-dx
-    y1 = props[0].centroid[1]+dy
+        # get end
+        x2 = props[i].centroid[0]+dx
+        y2 = props[i].centroid[1]-dy
 
-    # get end
-    x2 = props[0].centroid[0]+dx
-    y2 = props[0].centroid[1]-dy
+        # plot line
+        cv2.line(img, (int(y2), int(x2)), (int(y1), int(x1)), (255, 0, 255), 2)
 
-    # plot line
-    cv2.line(img, (int(y2), int(x2)), (int(y1), int(x1)), (255, 0, 255), 2)
+        # get major and minor axis
+        major = props[i].major_axis_length
+        minor = props[i].minor_axis_length
 
-    # get major and minor axis
-    major = props[0].major_axis_length
-    minor = props[0].minor_axis_length
+        # add perimeter of mask
+        perimeter = props[i].perimeter
 
-    # add perimeter of mask
-    perimeter = props[0].perimeter
+        # add area of mask
+        area = props[i].area
 
-    # add area of mask
-    area = props[0].area
+        # add solidity (proportion of the pixels in shape to the pixels in the convex hull)
+        solidity = props[i].solidity
 
-    # add solidity (proportion of the pixels in shape to the pixels in the convex hull)
-    solidity = props[0].solidity
+        # Create ID for image with image number and base directory
+        imgnum = os.path.basename(image)
+        imgdir = image.split(os.path.sep)[-2]
+        ID = os.path.join(imgdir, imgnum)
 
-    # Create ID for image with image number and base directory
-    imgnum = os.path.basename(image)
-    imgdir = image.split(os.path.sep)[-2]
-    ID = os.path.join(imgdir, imgnum)
+        # scale measurements back to original image size where necessary
+        perimeter = perimeter/scf
+        area = area / scf**2
+        minor = minor/scf
+        major = major/scf
 
-    # scale measurements back to original image size where necessary
-    perimeter = perimeter/scf
-    area = area / scf**2
-    minor = minor/scf
-    major = major/scf
-
+    
     # create dictionary with results
     res = {}
     res['ID'] = ID
