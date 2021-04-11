corrected wrong scaling

measurement_methods.py
@@ -49,14 +49,14 @@ def head_method(image):
     gray = np.uint8(np.mean(img, 2))
     '''
     # import and resize
-    img = utils.import_image(image)
+    img_res = utils.import_image(image)
 
-    # grayscale
-    gray = utils.grayscale_image(img)
-
-    # export width, height and scaling factor
-    height, width = np.shape(img)[0:2]
-    scf = 720/width
+    # define output into different variables
+    img = img_res["img"]
+    gray = img_res["gray"]
+    height = img_res["height"]
+    width = img_res["width"]
+    scf = img_res["scf"]
 
     # create mask
     edges_mag = scharr(gray)

utils.py
@@ -13,7 +13,8 @@ import warnings
 # Create function to load image in proper format
 def import_image(image):
     '''
-    imports and resizes image
+    imports, resizes and grayscales image.
+     returns additional info on image.
     '''
     # Load in rayscale, resize
     img = cv2.imread(image)
@@ -26,16 +27,15 @@ def import_image(image):
     nheight = int(height * scf)
     img = cv2.resize(img, (nwidth, nheight))
 
-    # export image
-    return(img)
-
-# write function to grayscale image
-def grayscale_image(img):
-    '''
-    exports image to grayscale
-    '''
     # grayscale image
     gray = np.uint8(np.mean(img, 2))
 
-    # export grayscale
-    return(gray)
+    # create dictionary with results
+    res = {}
+    res["img"] = img
+    res["gray"] = gray
+    res["height"] = height
+    res["width"] = width
+    res["scf"] = scf
+    # export image
+    return(res)