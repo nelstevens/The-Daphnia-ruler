c25df538f8468986205c60e6e0101367a88e8d11
nelstevens <nelson.stevens92@gmail.com>
2021-09-17 15:18:06 +0200

    added all variants of argument combination in daphnia ruler test

--- tests/test_daphnia_ruler.py
+++ tests/test_daphnia_ruler.py
@@ -5,12 +5,111 @@ import os
 import pytest
 # create directory paths
 pths = [os.path.join(os.getcwd(), "tests", "test_dirs", "test_ruler"), os.path.join(os.getcwd(), "tests", "test_dirs", "test_ruler_recursive")]
-# test main
+
+# test main without other options
 @pytest.mark.parametrize("dirs", pths)
 def test_main(monkeypatch, dirs):
     # set sys.argv correctly
     with monkeypatch.context() as m:
         m.setattr(sys, "argv", ["daphnia_ruler.py", "-p", dirs])
+
+        # assert no error
+        try:
+            dr.main()
+            assert True
+        except Exception:
+            assert False
+
+# test main with eye method enabled
+@pytest.mark.parametrize("dirs", pths)
+def test_main_eye(monkeypatch, dirs):
+    # set sys.argv correctly
+    with monkeypatch.context() as m:
+        m.setattr(sys, "argv", ["daphnia_ruler.py", "-e", "-p", dirs])
+
+        # assert no error
+        try:
+            dr.main()
+            assert True
+        except Exception:
+            assert False
+
+# test main with eye method and scale enabled
+@pytest.mark.parametrize("dirs", pths)
+def test_main_eye_scale(monkeypatch, dirs):
+    # set sys.argv correctly
+    with monkeypatch.context() as m:
+        m.setattr(sys, "argv", ["daphnia_ruler.py", "-e", "-s", "-p", dirs])
+
+        # assert no error
+        try:
+            dr.main()
+            assert True
+        except Exception:
+            assert False
+
+# test main with eye method, scale and no images enabled
+@pytest.mark.parametrize("dirs", pths)
+def test_main_eye_scale_no(monkeypatch, dirs):
+    # set sys.argv correctly
+    with monkeypatch.context() as m:
+        m.setattr(sys, "argv", ["daphnia_ruler.py", "-e", "-s", "-n", "-p", dirs])
+
+        # assert no error
+        try:
+            dr.main()
+            assert True
+        except Exception:
+            assert False
+
+# test main with scale enabled only
+@pytest.mark.parametrize("dirs", pths)
+def test_main_scale(monkeypatch, dirs):
+    # set sys.argv correctly
+    with monkeypatch.context() as m:
+        m.setattr(sys, "argv", ["daphnia_ruler.py", "-s", "-p", dirs])
+
+        # assert no error
+        try:
+            dr.main()
+            assert True
+        except Exception:
+            assert False
+
+# test main with scale and no images enabled only
+@pytest.mark.parametrize("dirs", pths)
+def test_main_scale_no(monkeypatch, dirs):
+    # set sys.argv correctly
+    with monkeypatch.context() as m:
+        m.setattr(sys, "argv", ["daphnia_ruler.py", "-s", "-n", "-p", dirs])
+
+        # assert no error
+        try:
+            dr.main()
+            assert True
+        except Exception:
+            assert False
+
+# test main with scale and no images enabled only
+@pytest.mark.parametrize("dirs", pths)
+def test_main_no(monkeypatch, dirs):
+    # set sys.argv correctly
+    with monkeypatch.context() as m:
+        m.setattr(sys, "argv", ["daphnia_ruler.py", "-n", "-p", dirs])
+
+        # assert no error
+        try:
+            dr.main()
+            assert True
+        except Exception:
+            assert False
+
+# test main with scale and no images enabled only
+@pytest.mark.parametrize("dirs", pths)
+def test_main_eye_no(monkeypatch, dirs):
+    # set sys.argv correctly
+    with monkeypatch.context() as m:
+        m.setattr(sys, "argv", ["daphnia_ruler.py", "-e", "-n", "-p", dirs])
 
         # assert no error
         try:
