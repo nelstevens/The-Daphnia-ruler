2923a1f6c3e1091b8d842d9e5cd18dbf52ab7038
nelstevens <nelson.stevens92@gmail.com>
2021-09-18 13:16:01 +0200

    added tests for scale_measurement

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,9 @@
 import helpers
 import pytest
 import os
+import numpy as np
+import sys
+import daphnia_ruler as dr
 
 # test if measure_except_eye behaves properly on different images
 def test_measure_except_eye():
@@ -58,3 +61,56 @@ def test_check_scale():
     # expect error
     with pytest.raises(ValueError):
         helpers.check_scale(pth)
+
+# test scale measurement function
+def test_scale_measurements_eye(monkeypatch):
+    # set sys.argv correctly
+    with monkeypatch.context() as m:
+        m.setattr(sys, "argv", ["daphnia_ruler.py", "-p", "./tests/test_dirs/test_images", "-s", "-e"])
+        # load input array
+        inarr = np.load(os.path.join(os.getcwd(), "tests", "test_arrays", "scale_measurement_array.npy"), allow_pickle = True)
+        actual = helpers.scale_measurements(
+            res = inarr, 
+            img_dir = ".",
+            sc_factor = 0.032,
+            args = dr.parse_args(sys.argv)
+        )
+        # load expected
+        expected = np.load(os.path.join(os.getcwd(), "tests", "test_arrays", "expected_scale_measurement_array.npy"), allow_pickle = True)
+
+        # assert correct for length in sample1
+        assert actual[0]["eye.Length"] == expected[0]["eye.Length"]
+        # assert exception for eye length in sample 3
+        with pytest.raises(KeyError):
+            assert actual[1]["eye.Length"] == expected[1]["eye.Length"]
+        # assert correct for tail length in sample1
+        assert actual[0]["tail.Length"] == expected[0]["tail.Length"]
+        # assert exception for eye length in sample 3
+        with pytest.raises(KeyError):
+            assert actual[1]["tail.Length"] == expected[1]["tail.Length"]
+        
+        # assert correctness of rest
+        for key in ["perimeter", "area", "minor", "full.Length"]:
+            assert actual[0][key] == expected[0][key]
+            assert actual[1][key] == expected[1][key]
+
+# test scale measurement function withou eye method
+def test_scale_measurements_head(monkeypatch):
+    # set sys.argv correctly
+    with monkeypatch.context() as m:
+        m.setattr(sys, "argv", ["daphnia_ruler.py", "-p", "./tests/test_dirs/test_images", "-s"])
+        # load input array
+        inarr = np.load(os.path.join(os.getcwd(), "tests", "test_arrays", "scale_measurement_array.npy"), allow_pickle = True)
+        actual = helpers.scale_measurements(
+            res = inarr, 
+            img_dir = ".",
+            sc_factor = 0.032,
+            args = dr.parse_args(sys.argv)
+        )
+        # load expected
+        expected = np.load(os.path.join(os.getcwd(), "tests", "test_arrays", "expected_scale_measurement_array.npy"), allow_pickle = True)
+        
+        # assert correctness of rest
+        for key in ["perimeter", "area", "minor", "full.Length"]:
+            assert actual[0][key] == expected[0][key]
+            assert actual[1][key] == expected[1][key]
