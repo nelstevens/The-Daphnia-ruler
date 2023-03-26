873b0935e12ee5416470db30304fab01572cce14
nelstevens <nelson.stevens92@gmail.com>
2023-03-26 12:21:28 +0200

    bump to v0.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="daphruler",
-    version="0.4.0",
+    version="0.4.1",
     author="Nelson Stevens",
     author_email="nelson.stevens92@gmail.com",
     description="Automate collecting morphometric traits of zooplankton.",
